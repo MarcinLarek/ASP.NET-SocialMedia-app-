c221dc530236337fc5c71b4cbfc9bd6e108482d4
MarcinLarek <marcinlarek00@gmail.com>
2023-01-20 15:32:29 +0100

    Auth do podstron

--- ProjektZaliczeniowy/Controllers/PostsController.cs
+++ ProjektZaliczeniowy/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,7 @@ using ProjektZaliczeniowy.Models;
 
 namespace ProjektZaliczeniowy.Controllers
 {
+    [Authorize]
     public class PostsController : Controller
     {
         private readonly ApplicationDbContext _context;
